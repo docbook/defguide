b6963d8b330b3378fc43037889259a964a266431
kosek <kosek@dd7a81d6-0b7c-4c3e-ac6c-c1c067ae8ec9>
2002-06-13 22:18:10 +0000

    Added very basic support for automatic build of CHMs on Windows box.
    git-svn-id: file:///tmp/svnrepo/defguide/trunk@103 dd7a81d6-0b7c-4c3e-ac6c-c1c067ae8ec9

---
+++ defguide/en/makechm.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+SET HHC="c:\Program Files\HTML Help Workshop\hhc.exe"
+REM Sorry for MS-DOS batch file, but you probably never will be able to produce CHMs on Unix box
+REM Script expects that saxon command is able to run Saxon processor and that your copy 
+REM of HTML Help Workshop is installed in common place
+
+saxon book.xml stylesheets/htmlhelp.xsl
+%HHC% htmlhelp.hhp
+
+saxon book.xml stylesheets/htmlhelp.xsl "output.type=unexpanded"
+%HHC% htmlhelp.hhp
+
+
