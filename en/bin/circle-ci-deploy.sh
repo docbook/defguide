9b280d58515eee73c558153736b5a2a4288c03d1
Norman Walsh <ndw@nwalsh.com>
2016-11-15 20:48:41 -0600

    Hacking a deploy script

--- en/bin/circle-ci-deploy.sh
+++ en/bin/circle-ci-deploy.sh
@@ -4,12 +4,18 @@ BOOKVERSION=`cat gradle.properties | grep "^bookVersion" | cut -f2 -d=`
 DBVERSION=`cat gradle.properties | grep "^docbookVersion" | cut -f2 -d=`
 DBVERSION=`echo $DBVERSION | cut -f1 -db` # ignore b1, b2, ... suffixes
 DBVERSION=`echo $DBVERSION | cut -f1 -dC` # ignore CR1, CR2, ... suffixes
+DBVERSION=`echo $DBVERSION | cut -f2 -dV` # ignore the leading V
 BUILD=`pwd`/build
 
 mkdir /home/ubuntu/staging
 cd /home/ubuntu/staging
 git clone --branch=gh-pages git@github.com:ndw/defguide.git gh-pages
-ls
+git rm -rf ./${DBVERSION}
+mkdir -p ./${DBVERSION}
+cp -Rf $BUILD/html/* ./${DBVERSION}/
+git add -f .
+git commit -m "CircleCI build: $CIRCLE_BUILD_URL"
+git push -fq origin gh-pages
 
 echo "Where am I?"
 pwd
