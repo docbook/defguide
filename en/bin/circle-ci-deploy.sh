b0b3b9bcee9e76d0fb69868e7f1b1334937e48e5
Norman Walsh <ndw@nwalsh.com>
2016-11-16 07:46:30 -0600

    Create CNAME

--- en/bin/circle-ci-deploy.sh
+++ en/bin/circle-ci-deploy.sh
@@ -9,15 +9,25 @@ BUILD=`pwd`/build
 
 mkdir $HOME/staging
 cd $HOME/staging
+
 git config --global user.email "ndw@nwalsh.com"
 git config --global user.name "Norman Walsh"
 git clone --branch=gh-pages git@github.com:ndw/defguide.git gh-pages
+
 cd gh-pages
+
+# Set this in the CircleCI "Settings/Environment Variables" section
+if [ "$GITHUB_CNAME" != "" ]; then
+    echo $GITHUB_CNAME > CNAME
+fi;
+
 if [ -d ./tdg/{$DBVERSION} ]; then
     git rm -rf ./tdg/${DBVERSION}
 fi
+
 mkdir -p ./tdg/${DBVERSION}
 cp -Rf $BUILD/html/* ./tdg/${DBVERSION}/
+
 git add -f .
 git commit -m "CircleCI build: $CIRCLE_BUILD_URL"
 git push -fq origin gh-pages
