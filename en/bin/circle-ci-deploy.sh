90772d4f102f14dd53f945a17a9bf3166798cb55
Norman Walsh <ndw@nwalsh.com>
2016-11-15 20:39:55 -0600

    Hacking a deploy script

--- en/bin/circle-ci-deploy.sh
+++ en/bin/circle-ci-deploy.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+BOOKVERSION=`cat gradle.properties | grep "^bookVersion" | cut -f2 -d=`
+DBVERSION=`cat gradle.properties | grep "^docbookVersion" | cut -f2 -d=`
+DBVERSION=`echo $DBVERSION | cut -f1 -db` # ignore b1, b2, ... suffixes
+DBVERSION=`echo $DBVERSION | cut -f1 -dC` # ignore CR1, CR2, ... suffixes
+BUILD=`pwd`/build
+
+mkdir /home/ubuntu/staging
+cd /home/ubuntu/staging
+git clone --branch=gh-pages git@github.com:ndw/defguide.git gh-pages
+ls
+
 echo "Where am I?"
 pwd
 echo "What's here?"
 ls
 echo "What's the config?"
 set | grep CIRCLE
+echo "Home"
+echo $HOME
 
 exit 1
